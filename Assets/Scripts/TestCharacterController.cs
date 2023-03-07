6afdbb9ce4cf0b5df0fbd19aebf4ee231d807f32
Eric <93659312+EricTheArtist@users.noreply.github.com>
2023-03-07 07:36:39 +0200

    Initial Push

---
+++ Assets/Scripts/TestCharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleInputNamespace
+{
+    public class TestCharacterController : MonoBehaviour
+    {
+        public float movementSpeed = 10f;
+        public SpawnManager spawnManager;
+
+        public SteeringWheel SW;
+
+        private float hMovement;
+
+        void Update()
+        {
+            if (transform.position.x > 4.2f && SW.Angle > 0)
+            {
+                hMovement = 0 * movementSpeed / 100;
+            }
+            else if (transform.position.x < -4.2f && SW.Angle < 0)
+            {
+                hMovement = 0 * movementSpeed / 100;
+            }
+            else
+            {
+                hMovement = SW.Angle * movementSpeed / 50;
+            } 
+            float vMovement = 1 * movementSpeed;
+
+            transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            spawnManager.SpawnTriggerEnter();
+        }
+
+    }
+}
