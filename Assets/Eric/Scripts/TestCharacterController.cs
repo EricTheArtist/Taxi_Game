4f34323343ca1828e07c6d83c3a79f5b90168858
Eric Gardiner <ericgardiner10@gmail.com>
2023-03-16 16:10:44 +0000

    updated movement script

--- Assets/Eric/Scripts/TestCharacterController.cs
+++ Assets/Eric/Scripts/TestCharacterController.cs
@@ -1,37 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace SimpleInputNamespace
 {
     public class TestCharacterController : MonoBehaviour
     {
-        public float movementSpeed = 10f;
+
         public SpawnManager spawnManager;
-        
+
+        public bool SteeringWheel;
+
+        [Header("Steering Wheel")]
+        public float movementSpeed = 10f;
         public SteeringWheel SW;
-     
         private float hMovement;
+
+        [Header("Swiping")]
+        public float[] LanesX;
+        public int MoveToLane = 1;
+        public int MoveFromLane = 2;
+        public float hPosition = 0;
+
 
         void Update()
         {
-            if (transform.position.x > 4.2f && SW.Angle > 0) //ther 4.2 is the cordinqate position of the edge of the road
-            {
-                hMovement = 0 * movementSpeed / 100;
+            //Code for steering wheel
+            if (SteeringWheel== true) 
+                {
+                    if (transform.position.x > 4.2f && SW.Angle > 0) //the 4.2 is the cordinqate position of the edge of the road
+                {
+                    hMovement = 0 * movementSpeed / 100;
+                }
+                else if (transform.position.x < -4.2f && SW.Angle < 0)
+                {
+                    hMovement = 0 * movementSpeed / 100;
+                }
+                else
+                {
+                    hMovement = SW.Angle * movementSpeed / 100;
+                }  
             }
-            else if (transform.position.x < -4.2f && SW.Angle < 0)
-            {
-                hMovement = 0 * movementSpeed / 100;
-            }
-            else
-            {
-                hMovement = SW.Angle * movementSpeed / 100;
-            } 
-            float vMovement = 1 * movementSpeed;
 
+            //Code for swiping controls 
+
+
+            // set The forward movement
+            float vMovement = 1 * movementSpeed;
+            
+            //updates the position of the car with steering wheel included
             transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
 
+            //updates the position of the care with swiping
+            this.transform.position = new Vector3(hPosition,0.5f,transform.position.z);
+
         }
+
+
 
         private void OnTriggerEnter(Collider other)
         {
