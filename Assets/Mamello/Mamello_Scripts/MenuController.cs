de6df60f92f487a3536568b3f268facf91f8acbe
MarmaladeJam <53046693+MamelloRaboroko@users.noreply.github.com>
2023-03-29 15:09:12 +0100

    Added overhead robot to menu

--- Assets/Mamello/Mamello_Scripts/MenuController.cs
+++ Assets/Mamello/Mamello_Scripts/MenuController.cs
@@ -18,13 +18,21 @@ namespace SimpleInputNamespace
         public float movementSpeed = 10f;
         public SteeringWheel SW;
         private float hMovement;
+        [Header("Lights")]
         public GameObject SteeringwheelUI;
+        [SerializeField] private GameObject greenLight;
+        [SerializeField] private GameObject yellowLight;
+        [SerializeField] private GameObject redLight;
+       
+
+
 
         [Header("Swiping")]
         public float[] LanesX;
         public int MoveToLane = 1;
         public int MoveFromLane = 2;
         public float hPosition = 0;
+        public float hPositionCurrent = 0;
 
         public float swipeThreshold = 20f;
 
@@ -39,6 +47,11 @@ namespace SimpleInputNamespace
             {
                 SteeringwheelUI.SetActive(false);
             }
+
+            greenLight.SetActive(false);
+            yellowLight.SetActive(false);
+            redLight.SetActive(false);
+
         }
         void Update()
         {
@@ -111,17 +124,19 @@ namespace SimpleInputNamespace
                 transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
 
                 //updates the position of the car with swiping
-                this.transform.position = new Vector3(hPosition, 0.5f, transform.position.z);
+                //this.transform.position = new Vector3(hPosition, 0.5f, transform.position.z);
+                this.transform.position = new Vector3(Mathf.Lerp(hPositionCurrent, hPosition, 1f), 0.5f, transform.position.z);
             }
-           
+
 
         }
 
         void MovementTest()
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow) && MoveFromLane < 4)
+            if (Input.GetKeyDown(KeyCode.RightArrow) && MoveFromLane < 2)
             {
                 Debug.Log("Swiped Right");
+                hPositionCurrent = MoveFromLane;
                 MoveToLane = MoveFromLane + 1;
                 hPosition = LanesX[MoveToLane];
                 MoveFromLane = MoveToLane;
@@ -144,20 +159,36 @@ namespace SimpleInputNamespace
 
             if (other.tag == "Play")
             {
-                SceneManager.LoadScene("Game_Scene");
+                StartCoroutine("SceneSwitch");
+                greenLight.SetActive(true);
+                yellowLight.SetActive(false);
+                redLight.SetActive(false);
+                //SceneManager.LoadScene("Game_Scene");
             }
 
             if (other.tag == "Customisation")
             {
+                yellowLight.SetActive(true);
+                greenLight.SetActive(false);
+                redLight.SetActive(false);
                 print("Customisation");
             }
 
             if (other.tag == "Quit")
             {
-                Application.Quit();
+                redLight.SetActive(true);
+                greenLight.SetActive(false);
+                yellowLight.SetActive(false);
+                //Application.Quit();
                 print("Quit");
             }
 
+        }
+
+        IEnumerator SceneSwitch()
+        {
+            SceneManager.LoadScene("Game_Scene");
+            yield return new WaitForSeconds(20f);
         }
 
     }
