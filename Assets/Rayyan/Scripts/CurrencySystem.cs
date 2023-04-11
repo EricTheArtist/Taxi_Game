165541b2e8cde6235cfd99279fd1fca9c54caf8c
MarmaladeJam <53046693+MamelloRaboroko@users.noreply.github.com>
2023-04-11 12:02:39 +0100

    Coin animations & maintained volume of taxi stretch

--- Assets/Rayyan/Scripts/CurrencySystem.cs
+++ Assets/Rayyan/Scripts/CurrencySystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class CurrencySystem : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class CurrencySystem : MonoBehaviour
 
     public TMP_Text run_amount_text;
     public TMP_Text run_amount_shadow;
+
+    [SerializeField] private GameObject coinIcon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +47,7 @@ public class CurrencySystem : MonoBehaviour
     }
     public int Addition_Function(int coins_to_add, int current_coins)
     {
+        coinIcon.GetComponent<Animator>().SetTrigger("isRotating");
         ///Debug.Log(coins_to_add + "  " + current_coins);
         current_coins += coins_to_add;
         return current_coins;
