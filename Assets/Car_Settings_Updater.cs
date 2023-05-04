c674849fbd6d8fd3aa5a57e41b838a4ddeeb947f
Eric Gardiner <ericgardiner10@gmail.com>
2023-05-04 15:44:27 +0200

    added wheel alignment for player prefab

--- Assets/Car_Settings_Updater.cs
+++ Assets/Car_Settings_Updater.cs
@@ -9,6 +9,14 @@ public class Car_Settings_Updater : MonoBehaviour
     public Texture2D[] _CarMaskTex;
     public Renderer TaxiMaterial;
     int ActiveCar;
+
+    public GameObject TyresFront;
+    public GameObject TyresBack;
+
+    public float[] TyresFrontZ;
+    public float[] TyresBackZ;
+
+    public float[] TyresScaleX;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +40,12 @@ public class Car_Settings_Updater : MonoBehaviour
                 Meshes[i].SetActive(true);
                 TaxiMaterial.GetComponent<Renderer>().sharedMaterial.SetTexture("_Car_Tex", _CarBaseTex[i]);
                 TaxiMaterial.GetComponent<Renderer>().sharedMaterial.SetTexture("_Car_Mask", _CarMaskTex[i]);
+
+                TyresFront.transform.localPosition = new Vector3(0, -0.3267f, TyresFrontZ[i]);
+                TyresBack.transform.localPosition = new Vector3(0, -0.3268f, TyresBackZ[i]);
+
+                TyresFront.transform.localScale = new Vector3(TyresScaleX[i], 1, 1);
+                TyresBack.transform.localScale = new Vector3(TyresScaleX[i], 1, 1);
             }
             else
             {
