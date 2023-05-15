e3022c9fa3a91e7cf85adaa7ee2bdc6ab067a32e
Eric <93659312+EricTheArtist@users.noreply.github.com>
2023-05-15 15:14:02 +0200

    Updated environment and controller component of open world scene

--- Assets/OpenWorldCarController.cs
+++ Assets/OpenWorldCarController.cs
@@ -2,33 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenWorldCarController : MonoBehaviour
+namespace SimpleInputNamespace
 {
-    public float moveSpeed = 10f; // The speed at which the object will move
-    private bool isMoving = false; // A flag to keep track of whether the object is moving
-
-    public float rotationSpeed = 0.1f; // The speed at which the object will rotate
-    private float rotateInput; // The input value from GetAxis()
-
-
-    void Start()
+    public class OpenWorldCarController : MonoBehaviour
     {
-        
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W)) // Check if the player pressed the 'w' key
+        public float moveSpeed = 10f; // The speed at which the object will move
+        private bool isMoving = false; // A flag to keep track of whether the object is moving
+
+        public float rotationSpeed = 0.1f; // The speed at which the object will rotate
+        private float arrowInput; // The input value from GetAxis()
+        public SteeringWheel SWOW;
+
+
+
+        void Start()
         {
-            isMoving = true; // Set the flag to true to indicate that the object is moving
-        }
 
-        if (Input.GetKeyUp(KeyCode.W)) // Check if the player released the 'w' key
-        {
-            isMoving = false; // Set the flag to false to indicate that the object is no longer moving
         }
-    }
-    void FixedUpdate()
-    {
+        void Update()
+        {
+            arrowInput = SimpleInput.GetAxis("Vertical"); // Get the horizontal input value from the player
+
+
+
+            if (Input.GetKeyDown(KeyCode.W)||arrowInput == 1) // Check if the player pressed the 'w' key
+            {
+                isMoving = true; // Set the flag to true to indicate that the object is moving
+            }
+
+            if (Input.GetKeyUp(KeyCode.W)) // Check if the player released the 'w' key
+            {
+                isMoving = false; // Set the flag to false to indicate that the object is no longer moving
+            }
+        }
+        void FixedUpdate()
+        {
 
             if (isMoving) // Check if the object is moving
             {
@@ -36,14 +44,24 @@ public class OpenWorldCarController : MonoBehaviour
                 movement *= Time.fixedDeltaTime; // Scale the movement by the fixed time step
                 transform.position += movement; // Move the object in the calculated direction
 
-                rotateInput = SimpleInput.GetAxis("Horizontal"); // Get the horizontal input value from the player
-                transform.Rotate(Vector3.up * rotateInput * rotationSpeed); // Rotate the object around its up axis
+                arrowInput = SimpleInput.GetAxis("Vertical"); // Get the horizontal input value from the player
+                transform.Rotate(Vector3.up * (SWOW.Angle/100) * rotationSpeed); // Rotate the object around its up axis
 
             }
-        
-            
 
+
+
+        }
+
+        public void ButtonForward()
+        {
+            isMoving = true;
+            Debug.Log("Forward");
+        }
+
+        public void ButtonForwardUp()
+        {
+            isMoving = false;
+        }
     }
-
-
 }
