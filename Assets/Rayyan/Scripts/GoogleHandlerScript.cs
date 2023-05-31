187a1cb071c640ab66b41d42050133ece9aa0882
MemoryGhost <1839637@students.wits.ac.za>
2023-05-31 11:05:37 +0200

    GoogleHandlerScript

--- Assets/Rayyan/Scripts/GoogleHandlerScript.cs
+++ Assets/Rayyan/Scripts/GoogleHandlerScript.cs
@@ -1,18 +1,94 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GleyGameServices;
 using UnityEngine;
 
 public class GoogleHandlerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private LeaderboardNames[] allLeaderboards;//Stores all LeaderBaords
+    private AchievementNames[] allAchievements;//Stores all Achievements
+    private int indexNumberAchievements;
+    private int indexNumberLeaderboards;
+
+    private Test_HighScore highScoreHandler;
+
+    private long steeringHighscore = 0;
+    private long swipingHighscore = 0;
+
+    //check login status
+    private bool isLoggedInCheck = false;
+    [SerializeField] private GameObject Player;
+    private void Start()
+    {   
+        //call login method
+        if (!GameServices.Instance.IsLoggedIn())
+        {
+            //Login to Game Servicies
+                GameServices.Instance.LogIn(LoginResult);
+        }
+        else
+        {
+            isLoggedInCheck = true;
+        }
         
+        //add all leaderbaords to the list
+        int nrOfLeaderboards = System.Enum.GetValues(typeof(LeaderboardNames)).Length;
+        allLeaderboards = new LeaderboardNames[nrOfLeaderboards];
+        for (int i = 0; i < nrOfLeaderboards; i++)
+        {
+            allLeaderboards[i] = ((LeaderboardNames)i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void LoginResult(bool success)
+        {
+            if (success == true)
+            {
+                //Login was successful
+            }
+            else
+            {
+                //Login failed
+            }
+            Debug.Log("Login success: " + success);
+            GleyGameServices.ScreenWriter.Write("Login success: " + success);
+        }
+
+        void ShowLeaderboards()
+        {
+            GameServices.Instance.ShowLeaderboadsUI();
+            //GameServices.Instance.ShowSpecificLeaderboard(LeaderboardNames.TaxiRank);
+        }
+
+        void SwippingScoreHandler()
+        {
+            if (GameServices.Instance.IsLoggedIn())
+            {
+                GameServices.Instance.SubmitScore(swipingHighscore,allLeaderboards[indexNumberLeaderboards], ScoreSubmitted);
+            }
+            
+        } void SteeringScoreHandler()
+        {
+            if (GameServices.Instance.IsLoggedIn())
+            {
+                GameServices.Instance.SubmitScore(steeringHighscore,allLeaderboards[indexNumberLeaderboards], ScoreSubmitted);
+            }
+            
+        }
         
-    }
+        private void ScoreSubmitted(bool success, GameServicesError error)
+        {
+            if (success)
+            {
+                //score successfully submitted
+            }
+            else
+            {
+                //an error occurred
+                Debug.LogError("Score failed to submit: " + error);
+            }
+            Debug.Log("Submit score result: " + success + " message:" + error);
+            GleyGameServices.ScreenWriter.Write("Submit score result: " + success + " message:" + error);
+        }
 }
