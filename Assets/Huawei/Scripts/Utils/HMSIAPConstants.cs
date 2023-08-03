1f1581edbc35a74133cd0f6f65ef92a291e51bcd
Eric Gardiner <ericgardiner10@gmail.com>
2023-08-03 11:16:32 +0200

    generated huwawei IAP Data

---
+++ Assets/Huawei/Scripts/Utils/HMSIAPConstants.cs
@@ -0,0 +1,25 @@
+public class HMSIAPConstants
+{
+	public const string comvetkoekstudiostaxirankedcoinstiny = "com.vetkoekstudios.taxiranked.coinstiny";
+	public const string comvetkoekstudiostaxirankedcoinssmall = "com.vetkoekstudios.taxiranked.coinssmall";
+	public const string comvetkoekstudiostaxirankedcoinsmediumv2 = "com.vetkoekstudios.taxiranked.coinsmediumv2";
+	public const string comvetkoekstudiostaxirankedcoinslargev2 = "com.vetkoekstudios.taxiranked.coinslargev2";
+	public const string comvetkoekstudiostaxirankedcoinsextralargev2 = "com.vetkoekstudios.taxiranked.coinsextralargev2";
+	public const string comvetkoekstudiostaxirankedcoinsmassivev2 = "com.vetkoekstudios.taxiranked.coinsmassivev2";
+	public const string comvetkoekstudiostaxirankedcolour01 = "com.vetkoekstudios.taxiranked.colour01";
+	public const string comvetkoekstudiostaxirankedcolour02 = "com.vetkoekstudios.taxiranked.colour02";
+	public const string comvetkoekstudiostaxirankedcolour03 = "com.vetkoekstudios.taxiranked.colour03";
+	public const string comvetkoekstudiostaxirankedcolour04 = "com.vetkoekstudios.taxiranked.colour04";
+	public const string comvetkoekstudiostaxirankedcolour02top = "com.vetkoekstudios.taxiranked.colour02top";
+	public const string comvetkoekstudiostaxirankedcolour03top = "com.vetkoekstudios.taxiranked.colour03top";
+	public const string comvetkoekstudiostaxirankedcolour04top = "com.vetkoekstudios.taxiranked.colour04top";
+	public const string comvetkoekstudiostaxirankedrims01 = "com.vetkoekstudios.taxiranked.rims01";
+	public const string comvetkoekstudiostaxirankedrims02 = "com.vetkoekstudios.taxiranked.rims02";
+	public const string comvetkoekstudiostaxirankedrims03 = "com.vetkoekstudios.taxiranked.rims03";
+	public const string comvetkoekstudiostaxirankedrims04 = "com.vetkoekstudios.taxiranked.rims04";
+	public const string comvetkoekstudiostaxirankedrims05 = "com.vetkoekstudios.taxiranked.rims05";
+	public const string comvetkoekstudiostaxirankedrims06 = "com.vetkoekstudios.taxiranked.rims06";
+	public const string comvetkoekstudiostaxirankedcar02 = "com.vetkoekstudios.taxiranked.car02";
+	public const string comvetkoekstudiostaxirankedcar03 = "com.vetkoekstudios.taxiranked.car03";
+	public const string comvetkoekstudiostaxirankedcar04 = "com.vetkoekstudios.taxiranked.car04";
+}
