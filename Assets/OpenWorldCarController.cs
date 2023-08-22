3494d110c7b1040f02847ae6a868f8f1f1bc7814
Eric Gardiner <Ericgardiner10@gmail.com>
2023-08-22 16:13:19 +0200

    Improvement of car controller

--- Assets/OpenWorldCarController.cs
+++ Assets/OpenWorldCarController.cs
@@ -6,8 +6,12 @@ namespace SimpleInputNamespace
 {
     public class OpenWorldCarController : MonoBehaviour
     {
-        public float moveSpeed = 10f; // The speed at which the object will move
-        private bool isMoving = false; // A flag to keep track of whether the object is moving
+        public float acceleration = 0.5f;
+        float maxSpeed = 10f;
+        float maxReverse = 3f;
+        float moveSpeed = 1f; // The speed at which the object will move
+        private bool isMovingforward = false; // A flag to keep track of whether the object is moving
+        private bool isMovingbackward = false;
 
         public float rotationSpeed = 0.1f; // The speed at which the object will rotate
         private float arrowInput; // The input value from GetAxis()
@@ -27,41 +31,89 @@ namespace SimpleInputNamespace
 
             if (Input.GetKeyDown(KeyCode.W)||arrowInput == 1) // Check if the player pressed the 'w' key
             {
-                isMoving = true; // Set the flag to true to indicate that the object is moving
+                isMovingforward = true; // Set the flag to true to indicate that the object is moving
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                isMovingbackward = true;
             }
 
             if (Input.GetKeyUp(KeyCode.W)) // Check if the player released the 'w' key
             {
-                isMoving = false; // Set the flag to false to indicate that the object is no longer moving
+                isMovingforward = false; // Set the flag to false to indicate that the object is no longer moving
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                isMovingbackward = false;
             }
         }
         void FixedUpdate()
         {
-
-            if (isMoving) // Check if the object is moving
+            if (moveSpeed != 0)
             {
-                Vector3 movement = transform.forward * moveSpeed; // Calculate the movement vector in the object's forward direction
-                movement *= Time.fixedDeltaTime; // Scale the movement by the fixed time step
-                transform.position += movement; // Move the object in the calculated direction
-
                 arrowInput = SimpleInput.GetAxis("Vertical"); // Get the horizontal input value from the player
                 transform.Rotate(Vector3.up * (SWOW.Angle/100) * rotationSpeed); // Rotate the object around its up axis
+            }
+            if (isMovingforward) // Check if the object is moving
+            {
+                if (moveSpeed < maxSpeed)
+                {
+                    moveSpeed = moveSpeed + (Time.deltaTime + acceleration);
+                }
+                else
+                {
+                    moveSpeed = maxSpeed;
+                }
+                
+
 
             }
+            else if (isMovingbackward)
+            {
+                if (moveSpeed > -maxReverse)
+                {
+                    moveSpeed = moveSpeed - (Time.deltaTime + acceleration);
+                }
+                else
+                {
+                    moveSpeed = -maxReverse;
+                }
+            }
+            else
+            {
+                if(moveSpeed > 0)
+                {
+                    moveSpeed = moveSpeed - (Time.deltaTime + acceleration); 
+                }
+                else if (moveSpeed < 0)
+                {
+                    moveSpeed = moveSpeed + (Time.deltaTime + acceleration);
+                }
+                else
+                {
+                    moveSpeed = 0;
+                }
+                
+            }
 
+            Vector3 movement = transform.forward * moveSpeed; // Calculate the movement vector in the object's forward direction
+            movement *= Time.fixedDeltaTime; // Scale the movement by the fixed time step
+            transform.position += movement; // Move the object in the calculated direction
+
+ 
 
 
         }
 
         public void ButtonForward()
         {
-            isMoving = true;
+            isMovingforward = true;
             Debug.Log("Forward");
         }
 
         public void ButtonForwardUp()
         {
-            isMoving = false;
+            isMovingforward = false;
         }
     }
 }
